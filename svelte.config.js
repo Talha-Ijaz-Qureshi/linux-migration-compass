9a2a1cc0a0c5f93878931b70a3e27b4876729eca
talha <mrwcggr121@gmail.com>
2026-05-07 08:31:19 +0400

    maaaan no domain, but gh pages shoudl work

--- svelte.config.js
+++ svelte.config.js
@@ -4,23 +4,14 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-
 	kit: {
 		adapter: adapter({
 			pages: 'build',
-      		assets: 'build',
-      		fallback: 'index.html'
+			assets: 'build',
+			fallback: 'index.html'
 		}),
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? '/linux-migration-compass' : ''
-		prerender: {
-			handleHttpError: ({ path, referrer, message }) => {
-				// ignore deliberate link to non-existent page (used to generate 404)
-				if (path === '/not-found') return;
-				
-				// otherwise fail the build
-				throw new Error(message);
-			}
 		}
 	}
 };
